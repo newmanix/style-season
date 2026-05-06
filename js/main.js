3f4dd3592563b082c1aef78aeb205ab47784779a
Tee Hyatt <teehyatt@gmail.com>
2026-05-05 18:36:40 -0700

    Fixed issues, pushing to github

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
-//alert('test');
+function setSeason(season) {
+  let style = document.getElementById("seasonStyle");
+  let text = document.getElementById("seasonText");
+  let logo = document.getElementById("logo");
+  let clothing = document.getElementById("clothing");
 
-//https://tinyurl.com/dynamic-html-checker
-document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+  if (season === "spring") {
+    style.href = "css/spring.css";
+    text.textContent = "Fresh Spring Styles!";
+    logo.src = "images/spring.gif";
+    clothing.src = "images/spring-wear.jpg";
+  }
 
-document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);  
+  else if (season === "summer") {
+    style.href = "css/summer.css";
+    text.textContent = "Hot Summer Deals!";
+    logo.src = "images/summer.gif";
+    clothing.src = "images/summer-wear.jpg";
+  }
 
-//manages mobile nav 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}  
+  else if (season === "fall") {
+    style.href = "css/fall.css";
+    text.textContent = "Leap into our Fall Wear!";
+    logo.src = "images/fall.gif";
+    clothing.src = "images/fall-wear.jpg";
+  }
+
+  else if (season === "winter") {
+    style.href = "css/winter.css";
+    text.textContent = "Stay Warm This Winter!";
+    logo.src = "images/winter.gif";
+    clothing.src = "images/winter-wear.jpg";
+  }
+
+  else {
+    style.href = "css/default.css";
+    text.textContent = "Welcome to Eddie Browser!";
+    logo.src = "images/four-seasons.gif";
+    clothing.src = "images/300x400.png";
+  }
+}
